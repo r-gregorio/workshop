0bae4065696dcca5a102c071dce1b524665aaeeb
r-gregorio <r.gregorio@creativehope.co.jp>
2022-09-12 20:44:29 +0800

    section 8 and section 9 remaining

--- frozen/js/script.js
+++ frozen/js/script.js
@@ -1,14 +1,64 @@
 function readMore() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnReadMore = document.getElementById("btnReadMore");
+  var dots1 = document.getElementById("dots_1");
+  var moreText1 = document.getElementById("more_1");
+  var btnReadMore1 = document.getElementById("btnReadMore1");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnReadMore.innerHTML = "もっと読む"; 
-    moreText.style.display = "none";
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnReadMore1.innerHTML = "もっと読む"; 
+    moreText1.style.display = "none";
   } else {
-    dots.style.display = "none";
-    moreText.style.display = "inline";
+    dots1.style.display = "none";
+    btnReadMore2.innerHTML = "もっと読む"; 
+    moreText1.style.display = "inline";
   }
+
+}
+
+function readMore2() {
+  var dots2 = document.getElementById("dots_2");
+  var moreText2 = document.getElementById("more_2");
+  var btnReadMore2 = document.getElementById("btnReadMore2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnReadMore2.innerHTML = "もっと読む"; 
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnReadMore2.innerHTML = "もっと読む"; 
+    moreText2.style.display = "inline";
+  }
+}
+
+
+function readMore3() {
+  var dots3 = document.getElementById("dots_3");
+  var moreText3 = document.getElementById("more_3");
+  var btnReadMore2 = document.getElementById("btnReadMore2");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    btnReadMore2.innerHTML = "もっと読む"; 
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none";
+    btnReadMore2.innerHTML = "もっと読む"; 
+    moreText3.style.display = "inline";
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
